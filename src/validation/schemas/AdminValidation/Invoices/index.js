f87560c313b6f580516ff8a0a8a33b6134d6a08b
Hari Gopal Sharma <harigopal@valere.io>
2024-11-19 14:45:57 +0530

    fleet invoice complted

--- src/validation/schemas/AdminValidation/Invoices/index.js
+++ src/validation/schemas/AdminValidation/Invoices/index.js
@@ -12,16 +12,16 @@ module.exports = {
      */
     updateInvoice: Joi.object().keys({
 
-        status: Joi.string().optional().label('Status'), // Validate status if provided
-        sub_jobs: Joi.array().optional().items(Joi.object().keys({
+        status: Joi.string().required().allow('admin-updated', 'paid', 'overdue', 'vendor-updated').label('Status'), // Validate status if provided
+        sub_jobs: Joi.array().required().items(Joi.object().keys({
             sub_job_id: Joi.string().required().label('Sub Job ID'), // Validate sub job ID
             amount: Joi.number().required().label('Amount') // Validate amount
         })).label('Sub Jobs') // Validate sub_jobs as an array of objects
     }),
     updateFleetInvoice: Joi.object().keys({
 
-        status: Joi.string().optional().label('Status'), // Validate status if provided
-        sub_jobs: Joi.array().optional().items(Joi.object().keys({
+        status: Joi.string().required().allow('draft','sent', 'paid', 'overdue').label('Status'), // Validate status if provided
+        sub_jobs: Joi.array().required().items(Joi.object().keys({
             sub_job_id: Joi.string().required().label('Sub Job ID'), // Validate sub job ID
             amount: Joi.number().required().label('Amount') // Validate amount
         })).label('Sub Jobs'), // Validate sub_jobs as an array of objects
